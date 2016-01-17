66fb0d409f94e80d732873145db0f202787f4168
Etwyniel <beringer.aymeric@laposte.net>
2016-01-17 15:01:40 +0100

    Fixes?...

--- register.php
+++ register.php
@@ -8,36 +8,36 @@ $mysqli = new mysqli('sql4.freemysqlhosting.net', 'sql4103349', 'ugtSzWBZrY', 's
 $check_username = "SELECT * FROM credentials WHERE username = '$username'";
 $check_email = "SELECT * FROM credentials WHERE email = '$email'";
 if (strlen($username) < 4) {
-    header('Location :register_page.php?error=username_short');
+    header('Location: register_page.php?error=username_short');
     $mysqli->close();
     exit();
 } elseif (strlen($username) > 16) {
-    header('Location :register_page.php?error=username_long');
+    header('Location: register_page.php?error=username_long');
     $mysqli->close();
     exit();
 } elseif (strlen($password) < 8) {
-    header('Location :register_page.php?error=password_short');
+    header('Location: register_page.php?error=password_short');
     $mysqli->close();
     exit();
 } elseif (strlen($password) > 20) {
-    header('Location :register_page.php?error=password_long');
+    header('Location: register_page.php?error=password_long');
     $mysqli->close();
     exit();
 } elseif ($mysqli->query($check_username)->num_rows) {
-    header('Location :register_page.php?error=username_taken');
+    header('Location: register_page.php?error=username_taken');
     $mysqli->close();
     exit();
 } elseif ($mysqli->query($check_email)->num_rows) {
-    header('Location :register_page.php?error=email_taken');
+    header('Location: register_page.php?error=email_taken');
     $mysqli->close();
     exit();
 } else {
-    echo 'Free username!';
     $pwd = crypt($password, 'botsrule');
     $sql = "INSERT INTO credentials (username, password, email) VALUES ('$username', '$pwd', '$email')";
     $mysqli->query($sql);
     $mysqli->close();
-    header('Location :register_page.php?error=none');
+    echo 'still alive!';
+    header('Location: register_page.php?error=none');
     exit();
 }
 ?>
