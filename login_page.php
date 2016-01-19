a76bc81efebd684c55fa37e21ef1749fb0fb85c5
etwyniel <beringer.aymeric@laposte.net>
2016-01-19 12:52:40 +0100

    stoopid aside

--- login_page.php
+++ login_page.php
@@ -29,10 +29,6 @@
 			<a href="info.php"> Info</a>
 		</p>
 		<br>
-		<aside id="side">
-			<h3>Created by:</h3>
-			<ul><li><a href="http://euw.leagueoflegends.com">Etwyniel</a></li><li>Aymeric</li><li>Beringer</li><li><span style="font-size: 7px">Caillou</span></li></ul>
-		</aside>
 		<div id="main">
 			<br>
 			<h3>Log in using your credentials:</h3><br>
