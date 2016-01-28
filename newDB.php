7c5038ca6c0e5d6c7671ab0ba5c5da4a6e3997ef
etwyniel <beringer.aymeric@laposte.net>
2016-01-28 14:29:23 +0100

    Creating new message table
    Creating a new table on the database to store messages. Should consider removing this file afterwards.

--- newDB.php
+++ newDB.php
@@ -1,17 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "bdaea8ke";
+$servername = 'sql4.freemysqlhosting.net';
+$username = 'sql4103349';
+$password = 'ugtSzWBZrY';
+$database = 'sql4103349';
 
 
-$db = new mysqli($servername, $username, $password);
+/*$db = new mysqli($servername, $username, $password);
 if($db->query("CREATE DATABASE data")){
 
     echo 'works?<br>';
    "Failed creating new database: ".$db->connect_errno();
     die();
 
-}
+}*/
+
 echo 'Still alive<br>';
 
 // Create connection
@@ -22,15 +24,14 @@ if (!$conn) {
 }
 
 // Create database
-$sql = "CREATE DATABASE IF NOT EXISTS myDB";
-if (mysqli_query($conn, $sql)) {
+//$sql = "CREATE DATABASE IF NOT EXISTS myDB";
+//if (mysqli_query($conn, $sql)) {
     echo "Database created successfully<br>";
     mysqli_query($conn, 'USE myDB');
     echo mysqli_error($conn);
-    $sql = "CREATE TABLE IF NOT EXISTS credentials (
+    $sql = "CREATE TABLE IF NOT EXISTS message (
     username VARCHAR(16) NOT NULL PRIMARY KEY,
-    password CHAR(76) NOT NULL,
-    email VARCHAR(20) NOT NULL,
+    message VARCHAR(140) NOT NULL,
     reg_date TIMESTAMP
     )";
 
@@ -39,9 +40,9 @@ if (mysqli_query($conn, $sql)) {
     } else {
         echo 'Could not create table<br>'. mysqli_error($conn);
     }
-} else {
+/*} else {
     echo "Error creating database: " . mysqli_error($conn);
-}
+}*/
 
 mysqli_close($conn);
 ?> 
