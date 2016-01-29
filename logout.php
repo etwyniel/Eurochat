8bd9cdaa8078978570528d3fc6a1a9e8ff88f85c
etwyniel <beringer.aymeric@laposte.net>
2016-01-29 13:17:21 +0100

    Started commenting

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
+    //On détruit la session dans laquelle les identifiants sont stockés.
     session_start();
     session_destroy();
-    header("Location: index.php");
+    //On redirige l'utilisateur vers la page d'accueil.
+    header("Location: ..");
     exit;
 ?>
