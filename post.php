024c90f04500eb23ee89714b64f267eb41f1cc4c
etwyniel <beringer.aymeric@laposte.net>
2016-01-29 13:15:42 +0100

    Started commenting

--- post.php
+++ post.php
@@ -1,10 +1,15 @@
 <?php
+    //On récupère le nom de l'utilisateur et le message passés en POST
     $username = $_POST['username'];
     $message = htmlspecialchars($_POST['message']);
-
+    
+    //On crée une connexion à la base de données MySQL distante
     $mysqli = new mysqli('sql4.freemysqlhosting.net', 'sql4103349', 'ugtSzWBZrY', 'sql4103349');
+    //On crée un requête pour ajouter un message sur la base de données
     $query = "INSERT INTO message (username, message) VALUES ('$username', '$message')";
 
+    //Si la requête est effectuée sans problème, l'utilisateur est redirigé vers la page d'accueil.
+    //Sinon, il recoit un message d'erreur.
     if (mysqli_query($mysqli, $query)) {
         echo 'Success';
         header('Location: ..');
@@ -12,5 +17,6 @@
         echo 'Could not post message.<br>';
         echo mysqli_error($mysqli );
     }
+    //On ferme la connexion avec la base de données.
     mysqli_close($mysqli);
 ?>
