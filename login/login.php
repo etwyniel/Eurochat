183897001bfbc5caf47c2f8c761b233d7fd4b7fc
etwyniel <beringer.aymeric@laposte.net>
2016-02-16 13:10:22 +0100

    Commented

--- login/login.php
+++ login/login.php
@@ -1,33 +1,40 @@
 <?php
+    //On récupère le nom d'utilisateur, et on l'encode pour éviter les caractères spéciaux
     $username = htmlspecialchars($_POST['username'], ENT_QUOTES);
+    //On récupère le mot de passe
     $password = $_POST['password'];
-
+    
+    //On crée une connection à la base de donnée contenant les identifiants
     $mysqli = new mysqli('sql4.freemysqlhosting.net', 'sql4103349', 'ugtSzWBZrY', 'sql4103349');
-
+    
+    //On fait une requête avec le nom d'utilisateur
     $check_username = "SELECT * FROM credentials WHERE username = '$username'";
     $r = $mysqli->query($check_username);
     echo $r->num_rows;
     $a = $r->fetch_assoc();
-    echo '<br>' . $a['password'];
-    echo '<br>' . crypt($password, 'botsrule');
 
    if (!$username) {
+        //Si aucun nom d'utilisateur n'a été donné, on redirige l'utilisateur vers la page de connexion
         header("Location: ../login?error=no_username");
         $mysqli->close();
         exit();
     } elseif (!$password) {
+        //Si aucun mot de passe n'a été donné, on redirige l'utilisateur vers la page de connexion
         header("Location: ../login?error=no_pwd");
         $mysqli->close();
         exit();
     } elseif (!$r->num_rows) {
+        //Si on ne trouve pas le nom de l'utilisateur dans la base de données, on redirige l'utilisateur vers la page de connexion
         header('Location: ../login?error=unregistered');
         $mysqli->close();
         exit();
     } elseif ($a['password'] != crypt($password, 'botsrule')) {
+        //Si les mots de passe ne correspondent pas, on redirige l'utilisateur vers la page de connexion
         header('Location: ../login?error=wrong_pwd');
         $mysqli->close();
         exit();
     } else {
+        //Si les identifiants sont corrects, on connecte l'utilisateur au site et on le redirige vers ka page d'accueil
         session_start();
         $_SESSION['password'] = $password;
         $_SESSION['username'] = $username;
